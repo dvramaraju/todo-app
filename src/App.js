20bf72e5fd96fd91531fc617cf2549e1088f549e
dvramaraju <ramarajudv21@gmail.com>
2021-06-07 18:06:01 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,74 +2,86 @@ import React, { useState } from "react";
 import "./App.css";
 import { Button, Card, Form } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-function Todo({ todo, index, markTodo, removeTodo }) {
-  return (
-    <div className="todo">
-      <span
-        style={{
-          fontFamily: "kalam",
-          fontSize: "30px",
-          textDecoration: todo.isDone ? "line-through" : "",
-        }}
-      >
-        {todo.text}
-      </span>
-      <div>
-        <Button
-          variant="outline-success"
-          style={{ marginRight: "5px" }}
-          onClick={() => markTodo(index)}
-        >
-          ✓
-        </Button>
-        <Button variant="outline-danger" onClick={() => removeTodo(index)}>
-          ✕
-        </Button>
-      </div>
-    </div>
-  );
-}
-
-function FormTodo({ addTodo }) {
-  const [value, setValue] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!value) return;
-    addTodo(value);
-    setValue("");
-  };
-
-  return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group>
-        <Form.Label></Form.Label>
-        <Form.Control
-          type="text"
-          className="input"
-          style={{ fontFamily: "kalam", fontSize: 20 }}
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          placeholder="Add new todo"
-          autoComplete="off"
-        />
-      </Form.Group>
-      <Button variant="primary mb-3" type="submit">
-        Submit
-      </Button>
-    </Form>
-  );
-}
+// import Todo from "./Todo";
+// import FormTodo from "./FormTodo";
 
 function App() {
   const [todos, setTodos] = useState([
     {
-      text: "This is a sampe todo",
+      text: "This is a sample todo",
+      isDone: false,
+    },
+    {
+      text: "Another Todo",
       isDone: false,
     },
   ]);
 
+  function Todo({ todo, index, markTodo, editTodo, removeTodo }) {
+    return (
+      <div className="todo">
+        <span
+          style={{
+            fontFamily: "kalam",
+            fontSize: "30px",
+            textDecoration: todo.isDone ? "line-through" : "",
+          }}
+        >
+          {todo.text}
+        </span>
+        <div>
+          <Button
+            variant="outline-primary"
+            style={{ marginRight: "5px" }}
+            onClick={() => editTodo(index)}
+          >
+            ✏
+          </Button>
+          <Button
+            variant="outline-success"
+            style={{ marginRight: "5px" }}
+            onClick={() => markTodo(index)}
+          >
+            ✓
+          </Button>
+          <Button variant="outline-danger" onClick={() => removeTodo(index)}>
+            ✕
+          </Button>
+        </div>
+      </div>
+    );
+  }
+
+  function FormTodo({ addTodo }) {
+    const [value, setValue] = useState("");
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if (!value) return;
+      addTodo(value);
+      setValue("");
+    };
+
+    return (
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label></Form.Label>
+          <Form.Control
+            type="text"
+            className="input"
+            style={{ fontFamily: "kalam", fontSize: 20 }}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            placeholder="Add new todo"
+            autoComplete="off"
+          />
+        </Form.Group>
+        <Button variant="primary mb-3" type="submit">
+          Submit
+        </Button>
+      </Form>
+    );
+  }
 
   const addTodo = (text) => {
     const newTodos = [...todos, { text }];
@@ -78,7 +90,13 @@ function App() {
 
   const markTodo = (index) => {
     const newTodos = [...todos];
-    newTodos[index].isDone = true;
+    newTodos[index] = setTodos();
+    setTodos(newTodos);
+  };
+
+  const editTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index] = true;
     setTodos(newTodos);
   };
 
@@ -101,6 +119,7 @@ function App() {
                   index={index}
                   todo={todo}
                   markTodo={markTodo}
+                  editTodo={editTodo}
                   removeTodo={removeTodo}
                 />
               </Card.Body>
---
+++ src/FormTodo.js
@@ -0,0 +1,4 @@
+import { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+
+export default f
